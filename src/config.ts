46c737b96fc61f30e8736676e9c6ff80342cbb17
Ahmad Reza Zahedi <r.zahedi@gmail.com>
2022-12-06 20:32:14 -0600

    Project init

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Reza Zahedi, a Web Developer';
+export const SITE_DESCRIPTION = 'I\'m Reza Zahedi, a web developer trying to learn about web technologies everyday!';
