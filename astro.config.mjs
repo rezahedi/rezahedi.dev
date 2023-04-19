47081ffe671fbb02d9bed67b8c15fb11ce90ea84
Ahmad Reza Zahedi <r.zahedi@gmail.com>
2023-04-18 18:13:39 -0700

    Feat: Contact form created with SSR response and SendGrid.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: vercel()
 });
