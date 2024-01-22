c9d95e010d4f91a11c66030b653740df2b4cae0f
Reza Zahedi <r.zahedi@gmail.com>
2024-01-22 01:29:54 -0800

    feat: using rehype secionize on content for TOC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import sectionize from '@hbsnow/rehype-sectionize';
 
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
@@ -10,6 +11,9 @@ import preact from "@astrojs/preact";
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), preact()],
+  markdown:{
+    rehypePlugins: [sectionize],
+  },
   output: 'server',
   adapter: vercel()
 });
