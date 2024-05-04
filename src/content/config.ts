24097a4678851651e02472a8e86360cf08d860da
Reza Zahedi <r.zahedi@gmail.com>
2024-05-03 21:04:40 -0700

    Create projects collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,4 +17,26 @@ const blog = defineCollection({
   })
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  type: 'content',
+  schema: z.object({
+    layout: z.string(),
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.string(),
+    image: z.string(),
+    link: z.string(),
+    github: z.string(),
+    category: z.string(),
+    author: z.object({
+      name: z.string(),
+      handler: z.string(),
+      avatar: z.string(),
+      link: z.string(),
+    }),
+    tags: z.array(z.string())
+  })
+});
+
+
+export const collections = { blog, projects };
